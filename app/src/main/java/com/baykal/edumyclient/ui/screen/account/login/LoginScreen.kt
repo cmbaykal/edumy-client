12e7046b3b9448ac6a296a455dfaace1ded5e2a3
Mert Baykal <cmertbaykal@gmail.com>
2021-11-27 00:30:47 +0300

    - Add -> Login Screen

--- app/src/main/java/com/baykal/edumyclient/ui/screen/account/login/LoginScreen.kt
+++ app/src/main/java/com/baykal/edumyclient/ui/screen/account/login/LoginScreen.kt
@@ -1,48 +1,75 @@
 package com.baykal.edumyclient.ui.screen.account.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import android.util.Log
+import android.util.Patterns
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.baykal.edumyclient.ui.component.EButton
+import com.baykal.edumyclient.ui.component.ETextButton
+import com.baykal.edumyclient.ui.component.ETextField
+import com.baykal.edumyclient.ui.component.InputState
 import com.baykal.edumyclient.ui.navigation.Screen
 
 @Composable
 fun LoginScreen(navController: NavController? = null) {
 
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+    var email by remember { mutableStateOf(InputState()) }
+    var password by remember { mutableStateOf(InputState()) }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .padding(32.dp)
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
     ) {
-
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") }
-        )
-
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") }
-        )
-        Row {
-            Button(onClick = { navController?.navigate(Screen.Home.route) }) {
-                Text(text = "Login")
+        Box(
+            modifier = Modifier.weight(1.5f, true),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                fontSize = 40.sp,
+                fontWeight = FontWeight.ExtraBold,
+                text = "Edumy Mobile",
+            )
+        }
+        Column(
+            modifier = Modifier.weight(2.5f, true)
+        ) {
+            ETextField(
+                label = "E-mail",
+                onChange = { email = it },
+                success = { Patterns.EMAIL_ADDRESS.matcher(it).matches() },
+                errorText = "Boş olamaz"
+            )
+            ETextField(
+                label = "Password",
+                onChange = { password = it },
+                success = { it.length in 8..16 }
+            )
+            EButton(
+                text = "Login",
+            ) {
+                focusManager.clearFocus()
+                Log.d("LoginNavigation","mail : " + email.isError + " pass : " + password.isError)
+                if (!email.isError && !password.isError) {
+                    navController?.navigate(Screen.Home.route)
+                }
             }
-            Button(onClick = { navController?.navigate(Screen.Register.route) }) {
-                Text(text = "Register")
+            ETextButton(
+                text = "Don't you have an Edumy account?",
+            ) {
+                navController?.navigate(Screen.Register.route)
             }
         }
     }
