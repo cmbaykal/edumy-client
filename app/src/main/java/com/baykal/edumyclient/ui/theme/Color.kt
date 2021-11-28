e18f777322e100ed9ee229e0390fb8b679331759
Mert Baykal <cmertbaykal@gmail.com>
2021-11-28 18:59:44 +0300

    - Revise -> App generic Colors

--- app/src/main/java/com/baykal/edumyclient/ui/theme/Color.kt
+++ app/src/main/java/com/baykal/edumyclient/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.baykal.edumyclient.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkBackground = Color(0xFF222222)
+val Orange = Color(0xFFE07619)
+val OrangeVariant = Color(0x80E07619)
