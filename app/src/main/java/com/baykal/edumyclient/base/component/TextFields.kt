2f0fc7d2f9c4cabd710afbc0d7a1c3948c7e641e
Cihat Mert Baykal <cmertbaykal@gmail.com>
2022-03-07 12:46:45 +0300

    [DEV][Search View]

--- app/src/main/java/com/baykal/edumyclient/base/component/TextFields.kt
+++ app/src/main/java/com/baykal/edumyclient/base/component/TextFields.kt
@@ -1,15 +1,15 @@
 package com.baykal.edumyclient.base.component
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -27,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.baykal.edumyclient.base.extension.color
+import com.baykal.edumyclient.base.ui.theme.Gray
 import com.baykal.edumyclient.base.ui.theme.Orange
 import com.baykal.edumyclient.base.ui.theme.OrangeVariant
 
@@ -70,8 +71,8 @@ fun ETextField(
             value = text,
             onValueChange = {
                 text = it
-                isSuccess = success != null && success(it)
-                onChange(InputState(it, isSuccess))
+                isSuccess = success != null && success.invoke(it)
+                onChange.invoke(InputState(it, isSuccess))
             },
             label = {
                 Text(
@@ -129,6 +130,92 @@ fun ETextField(
                 color = "#ff6d64".color
             )
         }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun ESearchView(
+    modifier: Modifier = Modifier,
+    label: String,
+    value: String = "",
+    onChange: (String) -> Unit,
+    onAction: (() -> Unit)? = null,
+) {
+    var text by remember { mutableStateOf(value) }
+    var focused by remember { mutableStateOf(false) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val colors = TextFieldDefaults.textFieldColors(
+        focusedLabelColor = Orange,
+        unfocusedLabelColor = Color.Gray,
+        errorLabelColor = "#ff6d64".color,
+        focusedIndicatorColor = Orange,
+        unfocusedIndicatorColor = OrangeVariant,
+        errorIndicatorColor = "#ff6d64".color,
+        errorTrailingIconColor = "#ff6d64".color
+    )
+
+    Column(
+        Modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            modifier = modifier
+                .then(
+                    Modifier
+                        .fillMaxWidth()
+                        .onFocusChanged { focused = it.isFocused }
+                ),
+            colors = colors,
+            shape = RoundedCornerShape(50.dp),
+            value = text,
+            onValueChange = {
+                text = it
+                onChange.invoke(it)
+            },
+            placeholder = {
+                Text(text = label)
+            },
+            trailingIcon = {
+                Row(
+                    modifier = Modifier.padding(end = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                            .size(40.dp)
+                            .clickable(
+                                onClick = {
+                                    if (text.isNotEmpty()) {
+                                        text = ""
+                                        onChange.invoke(text)
+                                    }
+                                },
+                                indication = rememberRipple(color = OrangeVariant, radius = 18.dp),
+                                interactionSource = remember { MutableInteractionSource() },
+                            )
+                    ) {
+                        Icon(
+                            if (text.isEmpty()) Icons.Filled.Search else Icons.Filled.Close,
+                            tint = if (focused) Orange else Gray,
+                            contentDescription = "",
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = onAction?.let {
+                KeyboardActions {
+                    it.invoke()
+                    keyboardController?.hide()
+                }
+            } ?: run {
+                KeyboardActions.Default
+            }
+        )
     }
 }
 
