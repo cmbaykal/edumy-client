862453b73c0f596520e07e01950dee3025ef1950
Cihat Mert Baykal <cmertbaykal@gmail.com>
2022-03-20 16:30:10 +0300

    [DEV][List Component with Refreshing and Load More]

--- app/src/main/java/com/baykal/edumyclient/base/component/Lists.kt
+++ app/src/main/java/com/baykal/edumyclient/base/component/Lists.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ScrollableTabRow
 import androidx.compose.material.TabRowDefaults
@@ -17,7 +18,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.baykal.edumyclient.base.extension.isScrolledToEnd
 import com.baykal.edumyclient.base.ui.theme.Orange
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshState
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 @Composable
 fun ETabRow(
@@ -78,5 +83,66 @@ fun ETabRowItem(
             text = data,
             color = Orange
         )
+    }
+}
+
+@Composable
+fun <T> EList(
+    modifier: Modifier = Modifier,
+    isVertical: Boolean = true,
+    scrollState: LazyListState = rememberLazyListState(),
+    swipeRefreshState: SwipeRefreshState = rememberSwipeRefreshState(false),
+    swipeRefresh: Boolean = false,
+    onRefresh: () -> Unit = {},
+    loadMore: Boolean = false,
+    onLoadMore: () -> Unit = {},
+    loadMoreContent: @Composable () -> Unit = {},
+    items: MutableList<T> = mutableListOf(),
+    itemContent: @Composable (item: T) -> Unit
+) {
+    val scrollEndState by remember {
+        derivedStateOf {
+            with(scrollState) {
+                isScrolledToEnd()
+            }
+        }
+    }
+
+    LaunchedEffect(scrollEndState) {
+        if (scrollEndState && loadMore) {
+            onLoadMore.invoke()
+            scrollState.scrollToItem(scrollState.layoutInfo.totalItemsCount - 1)
+        }
+    }
+
+    SwipeRefresh(
+        modifier = modifier,
+        state = swipeRefreshState,
+        swipeEnabled = swipeRefresh,
+        onRefresh = onRefresh
+    ) {
+        if (isVertical) {
+            LazyColumn(state = scrollState) {
+                items(items) { item ->
+                    itemContent.invoke(item)
+                }
+                item {
+                    if (scrollEndState && loadMore) {
+                        loadMoreContent()
+                    }
+                }
+            }
+        } else {
+            LazyRow(state = scrollState) {
+                items(items) { item ->
+                    itemContent.invoke(item)
+                }
+                item {
+                    if (scrollEndState && loadMore) {
+                        loadMoreContent()
+                    }
+                }
+            }
+        }
     }
 }
