49639743b60be9e2b95ed5ea0489c75745c062c1
Cihat Mert Baykal <cmertbaykal@gmail.com>
2022-03-31 09:36:36 +0300

    [DEV][Date extensions]

--- app/src/main/java/com/baykal/edumyclient/base/extension/InlineExtensions.kt
+++ app/src/main/java/com/baykal/edumyclient/base/extension/InlineExtensions.kt
@@ -6,12 +6,20 @@ import java.util.*
 
 inline val String.color get() = Color(android.graphics.Color.parseColor(this))
 
-inline val Date.string: String get() {
+inline val Date.toBirth: String
+    get() {
+        val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        return dateFormat.format(this)
+    }
+
+inline val Date.string: String
+    get() {
         val dateFormat = SimpleDateFormat("dd MMMM yyyy HH:mm", Locale.getDefault())
         return dateFormat.format(this)
     }
 
-inline val Date.toJson: String get() {
-    val dateFormat = SimpleDateFormat("dd.MM.yyyy HH.mm.ss", Locale.getDefault())
-    return dateFormat.format(this)
-}
+inline val Date.toJson: String
+    get() {
+        val dateFormat = SimpleDateFormat("dd.MM.yyyy HH.mm.ss", Locale.getDefault())
+        return dateFormat.format(this)
+    }
